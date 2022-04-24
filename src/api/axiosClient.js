a266e09e2534b70d470f045d01458c3f6e8cd7ff
ThangDev-99 <phanhuuthang78pht@gmail.com>
2022-04-24 07:03:02 +0700

    deploy

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,6 +1,6 @@
   const authEndpoint = "https://accounts.spotify.com/authorize?";
   const clientId = "a4fee2070be142cea7b771c86e30c034";
-  const redirectUri = "http://localhost:3000";
+  const redirectUri = "https://thangdev-99.github.io/SpotifyClone/";
   const scopes = ["user-library-read", "playlist-read-private"];
 
   export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
